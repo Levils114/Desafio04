63c25f2e6b08c68c8fbbe5ce205ace28c8885897
Levi Siebra <31908348+Levils114@users.noreply.github.com>
2020-04-16 07:31:54 -0300

    Add files via upload

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,11 @@
 import Transaction from '../models/Transaction';
 
+interface CreateAppointmentDTO{
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 interface Balance {
   income: number;
   outcome: number;
@@ -14,15 +20,31 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const allIncome = this.transactions.map(transaction => transaction.type === 'income' ? transaction.value: 0).reduce((acumulatedValue, currentValue) => acumulatedValue + currentValue,0);
+    const allOutcome = this.transactions.map(transaction => transaction.type === 'outcome' ? transaction.value: 0).reduce((acumulatedValue, currentValue) => acumulatedValue + currentValue,0);
+
+    const total = allIncome - allOutcome;
+
+    const balance = {
+      income: allIncome,
+      outcome: allOutcome,
+      total
+    };
+
+    return balance;
+
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateAppointmentDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
